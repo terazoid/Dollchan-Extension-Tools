0b8ccdf1d8fb870dfd02e053a018fd3ee1f717fe
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2017-06-27 23:38:00 +0300

    :pencil2: Added color options

--- js/index.js
+++ js/index.js
@@ -8,6 +8,8 @@ $(() => {
     return;
   }
 
+  $('.feature-color').colorpicker({ /*options...*/ });
+
   const getStorage = (key) => {
     return JSON.parse(window.localStorage.getItem(key) || '{}');
   }
@@ -19,41 +21,50 @@ $(() => {
   const currentStyle = {};
   let selectedFeatures = getStorage('selectedFeatures');
 
-  const inputChecked = (input) => {
-    const checked = input.checked;
+  const inputChanged = (input) => {
     const feature = input.dataset.feature;
-    const style = input.dataset.style;
-    const disableQuery = input.dataset.disable;
-    const enableQuery = input.dataset.enable;
-    if (checked) {
-      $('head').append(`<style type="text/css" id=${ feature }>${ style }</style>`);
-      currentStyle[feature] = style;
-      selectedFeatures[feature] = true;
-    } else {
-      $(`#${ feature }`).remove();
+    const value = input.value || input.checked;
+    const def = input.dataset.default;
+    if (value == def || !value) {
+      $(`#${ feature }-style`).remove();
       delete currentStyle[feature];
       delete selectedFeatures[feature];
+    } else {
+      const style = input.dataset.style.replace('%s', value);
+      currentStyle[feature] = style;
+      selectedFeatures[feature] = value;
+      if ($(`#${ feature }-style`).html(style).length === 0) {
+        $('head').append(`<style type="text/css" id="${ feature }-style">${ style }</style>`);
+      }
     }
+    const disableQuery = input.dataset.disable;
     if (disableQuery) {
-      $(disableQuery).toggleClass('text-muted', checked)
-        .find('input').prop('disabled', checked);
+      $(disableQuery).toggleClass('text-muted', value)
+        .find('input').prop('disabled', value);
     }
+    const enableQuery = input.dataset.enable;
     if (enableQuery) {
-      $(enableQuery).toggleClass('text-muted', !checked)
-        .find('input').prop('disabled', !checked);
+      $(enableQuery).toggleClass('text-muted', !value)
+        .find('input').prop('disabled', !value);
     }
   }
 
   $('input[data-feature]').change((event) => {
-    inputChecked(event.target);
+    inputChanged(event.target);
     setStorage('selectedFeatures', selectedFeatures);
   });
 
   $('input[data-feature]').map((index, input) => {
     const feature = input.dataset.feature;
-    if (selectedFeatures[feature]) {
-      input.checked = true;
-      inputChecked(input);
+    const value = selectedFeatures[feature];
+    if (value) {
+      if (input.type === 'checkbox') {
+        input.checked = value;
+      } else {
+        input.value = value;
+        $(input).parent('.feature-color').colorpicker('setValue', value);
+      }
+      inputChanged(input);
     }
   });
 
